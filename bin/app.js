6bca705dcb115a6b669ee557a18dff5319ef1fad
Keita Moromizato <keita_moromizato@relationsgroup.co.jp>
2015-04-01 16:14:00 +0900

    Initial Commit

---
+++ bin/app.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var app = require('../lib/app');
+var program = require('commander');
+
+program.version('0.0.1')
+  .option('-e, --environment [environment_name]', 'Creting environment name')
+  .parse(process.argv);
+
+var envName = program.environment;
+
+app.create({envname: envName}, console.log, console.error);
